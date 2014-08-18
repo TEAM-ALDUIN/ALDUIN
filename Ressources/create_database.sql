b4424cc58e72063cf883b6bea3b332708ac34cc3
ttitto <ttitto@users.noreply.github.com>
2014-08-18 15:38:51 +0300

    sql create table Images added

--- Ressources/create_database.sql
+++ Ressources/create_database.sql
@@ -6,33 +6,40 @@ DEFAULT CHARACTER SET utf8;
 
 USE gallery;
 
-CREATE TABLE users (
+CREATE TABLE Users (
 ID int auto_increment not null,
 UserName nvarchar(50) not null,
 Pass nvarchar(60) not null,
 CONSTRAINT PK_Users PRIMARY KEY CLUSTERED(ID ASC)
 );
 
-
-
-
-CREATE TABLE img_albums(
+CREATE TABLE ImgAlbums(
 ID int auto_increment not null,
 UserId int not null,
 CatName nvarchar(100) not null,
 IsPublic boolean DEFAULT 0,
-Comment text,
-CONSTRAINT PK_Img_Catalogues PRIMARY KEY CLUSTERED(ID ASC),
-CONSTRAINT FK_Users_Img_Catalogues FOREIGN KEY (UserId) REFERENCES users(ID)
+CatComment text,
+CONSTRAINT PK_ImgAlbums PRIMARY KEY CLUSTERED(ID ASC),
+CONSTRAINT FK_Users_ImgAlbums FOREIGN KEY (UserId) REFERENCES Users(ID)
 );
 
+CREATE TABLE Images(
+ID int auto_increment not null,
+ImgName nvarchar(200) not null,
+ImgAlbumId int not null,
+ImgComment nvarchar(1000),
+Votes int DEFAULT 0,
+CONSTRAINT PK_Images PRIMARY KEY CLUSTERED(ID ASC),
+CONSTRAINT FK_Images_ImgAlbums FOREIGN KEY (ImgAlbumId) REFERENCES ImgAlbums(ID)
+);
 
 /*FILL TEST DATA*/
 
-INSERT INTO users(UserName, Pass) VALUES('ttitto','ttitto');
-INSERT INTO users(UserName, Pass) VALUES('martin','martin');
-INSERT INTO users(UserName, Pass) VALUES('katya','katya');
+INSERT INTO Users(UserName, Pass) VALUES('ttitto','ttitto');
+INSERT INTO Users(UserName, Pass) VALUES('martin','martin');
+INSERT INTO Users(UserName, Pass) VALUES('katya','katya');
 
-INSERT INTO img_albums(UserId, CatName,IsPublic, comment) VALUES (3,'my wedding',0,'ksadhflkashdfuhaspidufhpieurfgkajsdhfk;jasdh askefhiasudfhpiuasdhgia dsagusdiugfasd');
-INSERT INTO img_albums(UserId, CatName,IsPublic) VALUES (1,'my abitur',1);
-INSERT INTO img_albums(UserId, CatName,IsPublic) VALUES (4,'my crazy hollydays',1);
+INSERT INTO ImgAlbums(UserId, CatName,IsPublic, Catcomment) VALUES (3,'my wedding',0,'ksadhflkashdfuhaspidufhpieurfgkajsdhfk;jasdh askefhiasudfhpiuasdhgia dsagusdiugfasd');
+INSERT INTO ImgAlbums(UserId, CatName,IsPublic) VALUES (1,'my abitur',1);
+INSERT INTO ImgAlbums(UserId, CatName,IsPublic) VALUES (4,'my crazy hollydays',1);
+
