0fd8bc2ebcc8c25a56fe3cf00428c0a3991c074e
ttitto <ttitto@users.noreply.github.com>
2014-08-19 00:31:40 +0300

    register error fixed

--- AlduinGallery/inc/register.php
+++ AlduinGallery/inc/register.php
@@ -4,17 +4,13 @@ $err_password = "";
 if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['password-check'])) {
     if ($_POST['password'] === $_POST['password-check']) {
         $username = htmlentities($_POST['username']);
-
+            //TODO: check if the inserted user already exists
         $options = ['cost' => 11,];
         $passwordFromPost = $_POST['password'];
         $hash = password_hash($passwordFromPost, PASSWORD_BCRYPT, $options);
-        //TODO:insert into database...
-        mysqli_query($db,'Insert into Users(UserName, Pass) VALUES("ttitto79","ttitto89")');
-        $in= mysqli_query($db,'Select Username, Pass from Users');
-        print_r( $in->fetch_assoc());
+        mysqli_query($db,'Insert into Users(UserName, Pass) VALUES("'.$username.'","'.$hash.'")');
     } else {
         $err_password .= "Password doesn't match! ";
-        exit;
     }
 
 }
--- AlduinGallery/templates/register.php
+++ AlduinGallery/templates/register.php
@@ -1,7 +1,7 @@
 <?php
 include_once('./inc/register.php');
 ?>
-<form method="post" action="./inc/register.php" id="frm-register">
+<form method="post" action="" id="frm-register">
     <fieldset>
         <legend>Register</legend>
         <label for="username">Username</label>
