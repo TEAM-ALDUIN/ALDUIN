06d55c8beeee4e7cc2961467851f432d4dae8bfc
ttitto <ttitto@users.noreply.github.com>
2014-08-19 18:03:48 +0300

    database password updated

--- AlduinGallery/inc/common.php
+++ AlduinGallery/inc/common.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-$db=mysqli_connect('localhost','ttitto4_alduin','alduin','gallery') or die('No Server or database');
+$db=mysqli_connect('localhost','ttitto4_alduin','alduin','ttitto4_gallery') or die('No Server or database');
 mysqli_query($db, 'SET NAMES utf8');
 
