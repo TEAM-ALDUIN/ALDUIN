29afcc3eafbd9c456a59ca682c9d8259f3586b12
Katya Babacheva <kbabacheva@gmail.com>
2014-08-20 16:23:56 +0300

    showing random pics in gallery

--- AlduinGallery/templates/main-gallery.php
+++ AlduinGallery/templates/main-gallery.php
@@ -1,46 +1,43 @@
+        <?php
+
+        $dir ='images';
+        $file_display = array('jpg', 'jpeg', 'png', 'gif');
+        if (file_exists($dir) == false) {
+            echo 'Directory \''. $dir. '\' not found!';
+        } else {
+            $dir_contents = scandir($dir);
+
+            foreach ($dir_contents as $file) {
+                $file_type = strtolower(end(explode('.', $file)));
+
+                if ($file !== '.' && $file !== '..' && in_array($file_type, $file_display) == true)
+                {
+                    $pics[] = '<img src="'. $dir. '/'. $file. '" alt="'. $file. '" />';
+                }
+            }
+        }
+        shuffle($pics);
+        ?>
 <div class="imgGallery col-lg-7">
                 <div class="row">
                     <div class="col-xs-3">
                         <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
+                            <?php echo $pics[0];?>
                         </a>
                     </div>
                     <div class="col-xs-3">
                         <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
+                            <?php echo $pics[1];?>
                         </a>
                     </div>
                     <div class="col-xs-3">
                         <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
+							<?php echo $pics[2];?>
                         </a>
                     </div>
                     <div class="col-xs-3">
                         <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
-                        </a>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-xs-3">
-                        <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
-                        </a>
-                    </div>
-                    <div class="col-xs-3">
-                        <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
-                        </a>
-                    </div>
-                    <div class="col-xs-3">
-                        <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
-                        </a>
-                    </div>
-                    <div class="col-xs-3">
-                        <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
+                            <?php echo $pics[3];?>
                         </a>
                     </div>
                 </div>
@@ -48,22 +45,45 @@
                 <div class="row">
                     <div class="col-xs-3">
                         <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
+                            <?php echo $pics[4];?>
                         </a>
                     </div>
                     <div class="col-xs-3">
                         <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
+                            <?php echo $pics[5];?>
                         </a>
                     </div>
                     <div class="col-xs-3">
                         <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
+                            <?php echo $pics[6];?>
                         </a>
                     </div>
                     <div class="col-xs-3">
                         <a href="#" class="thumbnail">
-                            <img src="images/2.jpg">
+                            <?php echo $pics[7];?>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-3">
+                        <a href="#" class="thumbnail">
+                            <?php echo $pics[8];?>
+                        </a>
+                    </div>
+                    <div class="col-xs-3">
+                        <a href="#" class="thumbnail">
+                            <?php echo $pics[9];?>
+                        </a>
+                    </div>
+                    <div class="col-xs-3">
+                        <a href="#" class="thumbnail">
+                            <?php echo $pics[10];?>
+                        </a>
+                    </div>
+                    <div class="col-xs-3">
+                        <a href="#" class="thumbnail">
+                            <?php echo $pics[11];?>
                         </a>
                     </div>
                 </div>
